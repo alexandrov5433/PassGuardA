f3c93f81269a8874bfa70c01f91ed656e170361e
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-27 11:55:44 +0100

    Settings started.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,30 @@ import { RegisterComponent } from './account/register/register.component';
 import { HomeComponent } from './main/home/home.component';
 import { MainComponent } from './main/main.component';
 import { SettingsComponent } from './main/settings/settings.component';
+import { AccountComponent } from './main/settings/account/account.component';
+import { AppearanceComponent } from './main/settings/appearance/appearance.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/main/home', pathMatch: 'full' },
+    { path: 'main/settings', redirectTo: '/main/settings/account', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'main', component: MainComponent, children: [
-        { path: 'home', component: HomeComponent },
-        { path: 'settings', component: SettingsComponent},
-    ] },
-
+    {
+        path: 'main', component: MainComponent, children: [
+            { path: 'home', component: HomeComponent },
+            {
+                path: 'settings', component: SettingsComponent, children: [
+                    { path: 'account', component: AccountComponent },
+                    { path: 'appearance', component: AppearanceComponent }
+                ]
+            },
+        ]
+    },
+    { path: '**', redirectTo: noRouteRedirection},
+    
+    
 ];
+
+async function noRouteRedirection() {
+
+}
--- src/app/main/settings/settings.component.ts
+++ src/app/main/settings/settings.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
-  imports: [],
+  imports: [MatIconModule, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css',
   standalone: true
 })
 export class SettingsComponent {
 
+  constructor(
+    private iconReg: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+  ) {
+    this.iconReg.addSvgIcon(
+      'account',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('./account.svg')
+    );
+    this.iconReg.addSvgIcon(
+      'moon',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('./moon.svg')
+    );
+  }
 }
