817cf2365fe5f06fde067de10775bd8da9220f1d
alexandrov5433 <alexandrov5433@gmail.com>
2025-01-10 11:19:08 +0100

    Comments deleted.

--- build_installer.js
+++ build_installer.js
@@ -1,20 +1,13 @@
-// ./build_installer.js
-
-// 1. Import Modules
 const { MSICreator } = require('electron-wix-msi');
 const path = require('path');
 
-// 2. Define input and output directory.
-// Important: the directories must be absolute, not relative
 const APP_DIR = path.resolve(__dirname, './PassGuardA-win32-x64');
 const OUT_DIR = path.resolve(__dirname, './windows_installer');
 
-// 3. Instantiate the MSICreator
 const msiCreator = new MSICreator({
     appDirectory: APP_DIR,
     outputDirectory: OUT_DIR,
 
-    // Configure metadata
     description: 'An offline desktop password manager.',
     exe: 'PassGuardA',
     name: 'PassGuardA',
@@ -22,15 +15,11 @@ const msiCreator = new MSICreator({
     version: '1.0.0',
     icon: path.resolve('./electron/assets/passGuardAIcon256.ico'),
 
-    // Configure installer User Interface
     ui: {
         chooseDirectory: true
     },
 });
 
-// 4. Create a .wxs template file
 msiCreator.create().then(function(){
-
-    // Step 5: Compile the template to a .msi file
     msiCreator.compile();
 });
